b93a56d8232b1732540070e97e2d7eed4df2d537
alexskt123 <alex.kt.so@pccw.com>
2022-03-25 11:23:45 +0800

    initial version with create table scripts

--- config/index.js
+++ config/index.js
@@ -4,8 +4,15 @@ const config = {
     base: './output/',
     plantuml: './output/plantuml/',
     image: './output/diagram/',
+    sql: './output/sql/',
   },
   acceptXLSXSheetNames: ['Data Dict'],
+  mysqlConverter: [
+    { type: 'VARBINARY', mapper: 'RAW' },
+    { type: 'VARCHAR', mapper: 'VARCHAR2' },
+    { type: 'DECIMAL', mapper: 'NUMBER' },
+    { type: 'LONGTEXT', mapper: 'CLOB' },
+  ],
 };
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const config = require('./config');
-const { processXlsxFiles, outputUMLFiles, refreshOutputDir } = require('./lib/file');
+const { processXlsxFiles, outputFiles, refreshOutputDir } = require('./lib/file');
 const log = require('./lib/logger');
+const program = require('./lib/commander');
 
 try {
+  program
+    .description('An application for generating Diagrams/SQL files from an Excel file')
+    .option('--sql', 'Generate SQL scripts')
+    .option('--erd', 'Generate ER Diagrams');
+
+  program.parse();
+
+  const options = program.opts();
   const xlsxContent = processXlsxFiles();
 
   refreshOutputDir(config.outputPath.base);
-  outputUMLFiles(xlsxContent);
+  outputFiles(xlsxContent, options);
 
   log.info('Success!');
 } catch (e) {
